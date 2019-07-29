832e94bb5d22066b89b51bbff50ddbf7ed887278
Miki <makmel.miki@gmail.com>
2019-07-29 20:17:28 +0300

    Updated EventEngine

--- Final Project/SE_Methods/EventEngine.h
+++ Final Project/SE_Methods/EventEngine.h
@@ -1,24 +1,18 @@
 #pragma once
-#ifndef EVENT_ENGINE_H
-#define EVENT_ENGINE_H
 
-#include "Control.h"
 #include <windows.h>
 #include "Graphics.h"
+#include "Control.h"
 
 class EventEngine
 {
 public:
 	EventEngine(DWORD input = STD_INPUT_HANDLE, DWORD output = STD_OUTPUT_HANDLE);
-	void run(Control& c);
+	void run(Control&);
 	virtual ~EventEngine();
-
 private:
-	void moveFocus(Control &main, Control *focused);
-
+	void moveFocus(Control&, Control*);
 	Graphics _graphics;
 	HANDLE _console;
 	DWORD _consoleMode;
 };
-
-#endif
