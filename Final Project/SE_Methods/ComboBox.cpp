ebc1d204f0305cf93d887ac5c313aa80f1cc3169
Miki <makmel.miki@gmail.com>
2019-07-31 11:41:40 +0300

    Added ComboBox

--- Final Project/SE_Methods/ComboBox.cpp
+++ Final Project/SE_Methods/ComboBox.cpp
@@ -7,70 +7,61 @@ bool ComboBox::canGetFocus() { return TRUE; }
 bool ComboBox::isControlsList() { return TRUE; }
 int ComboBox::getWidth() { return width; }
 
-ComboBox::ComboBox(Border* border, short left, short top, vector<string> values) : Control(left, top, border), values(values), width(20), isOpen(FALSE), currentIndex(0)
-{
-	textBox = values[0];
-}
+ComboBox::ComboBox(Border* border, short left, short top, vector<string> values) : Control(left, top, border), values(values), width(18), isOpen(FALSE), currentIndex(0) { textBox = values[0]; }
 
-ComboBox::ComboBox(Border * border, short left, short top, vector<string> values, Color backgroundColor, Color foregroundColor) : Control(left, top, border), values(values), width(20), isOpen(FALSE), currentIndex(0)
+ComboBox::ComboBox(Border* border, short left, short top, vector<string> values, Color backgroundColor, Color foregroundColor) : Control(left, top, border), values(values), width(18), isOpen(FALSE), currentIndex(0)
 {
 	this->setBackgroundColor(backgroundColor);
 	this->setForegroundColor(foregroundColor);
 	textBox = values[0];
 }
 
-
 void ComboBox::draw(Graphics& graphics, int x, int y, size_t size)
 {
-	int vectorSize = static_cast<int>(values.size());
-	graphics.moveTo(x, y);
 	graphics.setBackground(backgroundColor);
 	graphics.setForeground(foregroundColor);
+	graphics.moveTo(x, y);
 
 	if (!isOpen)
 	{
 		cout << '\xDA';
-		for (int i = 0; i < 18; i++)
+		for (int i = 0; i < this->width; i++)
 			cout << '\xC4';
 		cout << '\xBF';
 		graphics.moveTo(x, y + 1);
 		cout << '\xB3';
-		for (int i = 0; i < 16; i++)
-		{
+		for (int i = 0; i < this->width-1; i++)
 			cout << ' ';
-		}
-		cout << '\xB3' << '\xF0' << '\xB3';
+		cout << 'v' << '\xB3';
 		graphics.write(x + 2, y + 1, textBox);
 		graphics.moveTo(x, y + 2);
 		cout << '\xC0';
-		for (int i = 0; i < 18; i++)
+		for (int i = 0; i < this->width; i++)
 			cout << '\xC4';
 		cout << '\xD9';
 	}
 	else
 	{
 		cout << '\xDA';
-		for (int i = 0; i < 18; i++)
-		{
+		for (int i = 0; i < this->width; i++)
 			cout << '\xC4';
-		}
 		cout << '\xBF';
 		graphics.moveTo(x, y + 1);
 		cout << '\xB3';
-		for (int i = 0; i < 16; i++)
+		for (int i = 0; i < this->width-1; i++)
 			cout << ' ';
-		cout << '\xB3' << '\xF0' << '\xB3';
+		cout << '^' << '\xB3';
 		graphics.write(x + 2, y + 1, values[0]);
 		graphics.moveTo(x, y + 2);
 		cout << '\xB3';
-		for (int i = 0; i < 18; i++)
+		for (int i = 0; i < this->width; i++)
 			cout << '\xC4';
 		cout << '\xB3';
 
 		int tempX = x;
 		int tempY = y + 3;
 
-		for (int y = 0; y < vectorSize; y++)
+		for (int y = 0; y < values.size(); y++)
 		{
 			graphics.setBackground(backgroundColor);
 			graphics.setForeground(foregroundColor);
@@ -79,52 +70,43 @@ void ComboBox::draw(Graphics& graphics, int x, int y, size_t size)
 				graphics.setBackground(Color::White);
 				graphics.setForeground(Color::Black);
 			}
-			if (y == vectorSize - 1)
+			if (y == values.size() - 1)
 			{
 				graphics.moveTo(tempX, tempY);
 				cout << '\xB3';
-				for (int x = 0; x < 18; x++)
-				{
+				for (int x = 0; x < this->width; x++)
 					cout << ' ';
-				}
 				cout << '\xB3';
 				graphics.write(tempX + 2, tempY, values[y]);
 				graphics.moveTo(tempX, ++tempY);
 				cout << '\xC0';
-				for (int x = 0; x < 18; x++)
-				{
+				for (int x = 0; x < this->width; x++)
 					cout << '\xC4';
-				}
 				cout << '\xD9';
 			}
 			else
 			{
 				graphics.moveTo(tempX, tempY);
 				cout << '\xB3';
-				for (int x = 0; x < 18; x++)
-				{
+				for (int x = 0; x < this->width; x++)
 					cout << ' ';
-				}
 				cout << '\xB3';
 				graphics.write(tempX + 2, tempY, values[y]);
 				graphics.moveTo(tempX, ++tempY);
 				cout << '\xB3';
-				for (int x = 0; x < 18; x++)
-				{
+				for (int x = 0; x < this->width; x++)
 					cout << '\xC4';
-				}
 				cout << '\xB3';
 				tempY++;
 			}
 
 		}
 	}
-
 }
 
 void ComboBox::mousePressed(int x, int y, bool isMousePressed)
 {
-	if (y == top && (x > left && x < (left + width - 1)))
+	if (y == top && (x > left && x < (left + this->width+1)))
 	{
 		if (!isOpen)
 		{
--- Final Project/SE_Methods/ComboBox.h
+++ Final Project/SE_Methods/ComboBox.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "Control.h"
+
 class ComboBox :public Control
 {
-private:
-	vector<string> values;
-	string textBox;
-	bool isOpen;
-	int currentIndex;
 	int width;
+	int currentIndex;
 	int selectedIndex;
+	bool isOpen;
+	string textBox;
+	vector<string> values;
+
 public:
 	ComboBox(Border*, short, short, vector<string>);
 	ComboBox(Border*, short, short, vector<string>, Color, Color);
 	void draw(Graphics&, int, int, size_t);
 	void mousePressed(int, int, bool);
 	void keyDown(int, char, Graphics&);
-	void onFocus(bool) {};
 	bool canGetFocus();
 	bool isControlsList();
 	bool isLastIndex();
